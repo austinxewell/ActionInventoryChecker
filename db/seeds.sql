2585bb16fd2058fad262fedb432e4e2ac914ed4d
Austin Ewell <austin.ewell86@gmail.com>
2022-09-20 00:20:07 -0600

    created basic mysql db, tables, and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO bins (bin, verified)
+VALUES
+    ('BIN 1', 1),
+    ('BIN 2', 1),
+    ('BIN 3', 0),
+    ('BIN 4', 1),
+    ('BIN 5', 1),
+    ('BIN 6', 1),
+    ('BIN 7', 0),
+    ('BIN 8', 0),
+    ('BIN 9', 1),
+    ('BIN 10', 1),
+    ('BIN 11', 0),
+    ('BIN 12', 1);
